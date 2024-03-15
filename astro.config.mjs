6e61230460f67b3598fa40253573aef571c361d2
labi1240 <labi1240@gmail.com>
2024-03-15 18:33:52 -0400

    feat: Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import { defineTable, setupAstroDB } from 'astro/db';
+import * as dbConfig from '../src/db/config.ts';
 
 // https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [setupAstroDB({
+      tables: dbConfig
+    })],
+  },
+});
 export default defineConfig({});
